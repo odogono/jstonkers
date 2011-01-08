f502fbc4eb5b265e21d8526f876961d363a95404
Alex Veenendaal <alex@odogono.com>
2011-01-08 22:10:05 +0000

    added an easier means of reading types from a buffer

---
+++ src/utils/data_buffer.js
@@ -0,0 +1,67 @@
+var util = require('util');
+
+var DataBuffer = exports.DataBuffer = function(data){
+    this.pos = 0;
+    this.data = data;
+}
+
+util.inherits(DataBuffer, Buffer);
+
+DataBuffer.prototype = {
+    
+    testBytes: function( b ){
+        for( var i=0;i<b.length;i++ ){
+            if( this.data[this.pos+i] != b[i] ){
+                return false;
+            }
+        }
+        this.pos += b.length;
+        return true;
+    },
+    
+    readInt: function() {
+        var result = (this.data[this.pos++]<<24) | (this.data[this.pos++]<<16) | (this.data[this.pos++]<<8) | (this.data[this.pos++]);
+        return result;
+    },
+    
+    readString: function(len) {
+        var result, i;
+        if( len ){
+            result = this.data.toString( 'ascii', this.pos, this.pos+len);
+            this.pos += len;
+        } else {
+            for( i=this.pos;i<this.pos+this.data.length;i++ ){
+                if( this.data[i] == 0 ){
+                    break;
+                }
+            }
+            result = this.data.toString( 'ascii', this.pos, i );
+            this.pos = i+1;
+        }
+        
+        return result;
+    },
+    
+    readBytes: function(len) {
+        var result = this.data.slice( this.pos, this.pos+len );
+        this.pos += len;
+        return result;
+    },
+    
+    readByte: function() {
+        return this.data[ this.pos++ ];
+    },
+    
+    finished: function() {
+        return this.pos >= this.data.length;
+    }
+}
+
+exports.create = function( data ) {
+    if( Array.isArray(data) )
+        data = new Buffer( data );
+    
+    var result = new DataBuffer( data );
+    
+    return result;
+}
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     'Vector2f': require('./vector2f'),
     'SocketIOClient': require('./socketio_client'),
+    'DataBuffer':require('./data_buffer'),
 };
