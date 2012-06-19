2c42560d5e58dc7dcab2f0292d53f4545b8f5b88
Alex Veenendaal <alex@opendoorgonorth.com>
2012-06-19 13:43:08 +0100

    readjusted import order

--- src/entity/index.js
+++ src/entity/index.js
@@ -1,7 +1,10 @@
 
 module.exports = require('./entity');
+
+module.exports.EntityCollection = require('./entity_collection').EntityCollection;
+
 _.extend(module.exports, require('./entity_relationship') );
 // _.extend(module.exports, require('./entity_server') );
 
-module.exports.EntityCollection = require('./entity_collection').EntityCollection;
-module.exports.createEntityCollection = require('./entity_collection').create;
+
+// module.exports.createEntityCollection = require('./entity_collection').create;
