7a4b4aad110962b0506aa1df81076487f38f7cd7
Alex Veenendaal <alex@opendoorgonorth.com>
2012-07-11 12:33:56 +0100

    added partials support into the mustache template renderer.

--- app/app.js
+++ app/app.js
@@ -16,21 +16,45 @@ program
 app.set('view options', {layout:'layouts/main'});
 app.set('view engine', 'mustache');
 
+// hack in partials support
+var existing = mustache.Renderer.prototype._partial;
+mustache.Renderer.prototype._partial = function(name,context){
+    // load the partial from file if the partial doesn't exist or the view cache is disabled
+    if( !this._partialCache[name] || !app.enabled('view cache') ){
+        var partialRaw = Common.fs.readFileSync( Common.path.join(app.settings.views,name+'.mustache'), 'utf8');
+        mustache.compilePartial( name, partialRaw );
+    }
+    return existing.apply( this, arguments );
+};
+
 app.engine('mustache', function(path, options, cb){
-    Common.fs.readFile(path, 'utf8', function(err, str){
-        if (err) return fn(err);
-        log('mustache ' + path + ' ' + JSON.stringify(options) );
-        try {
-            var renderFn = mustache.compile(str,options);
-            if( options.cb )
-                cb(null,renderFn);
-            else
-                cb(null,renderFn(options));
-        }
-        catch(err) {
-            cb(err);
-        }
-    });
+    mustache.templateCache = mustache.templateCache || {};
+
+    if( !mustache.templateCache[path] || !app.enabled('view cache') ){
+        Common.fs.readFile(path, 'utf8', function(err, str){
+            if (err) return fn(err);
+            try {
+                var renderFn = mustache.compile(str,options);
+                mustache.templateCache[path] = renderFn;
+                if( options.cb )
+                    cb(null,renderFn);
+                else
+                    cb(null,renderFn(options));
+            }
+            catch(err) {
+                cb(err);
+            }
+        });
+    } else {
+        if( options.cb )
+            cb(null, mustache.templateCache[path] );
+        else
+            cb(null, mustache.templateCache[path](options) );
+    }
+
+    // print_ins( app.settings );
+    // TODO - enable some kind of caching
+    
 });
 
 app.set('views', Common.path.join(__dirname,'views') );
@@ -61,6 +85,15 @@ require('./handlers/game_manager');
 app.configure( function(){
     Server.initialize();
     app.gameManager = Server.gameManager;
+    // app.enable('view cache')
+});
+
+app.configure('development', function(){
+
+});
+
+app.configure('production', function(){
+
 });
 
 var port = program.port || Common.config.server.port;
